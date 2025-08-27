b0826f2190eeefb056623204a47c8b5383388103
chatt <nodeadbatteries@gmail.com>
2025-08-26 17:44:13 -0700

    fix: prevent duplicate ids from resolving

--- src/web/routes/utils.rs
+++ src/web/routes/utils.rs
@@ -5,7 +5,7 @@ use crate::web::types::{
 };
 use rocket::State;
 use rocket::serde::json::Json;
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 use uuid::Uuid;
 
 pub async fn get_matches(
@@ -14,11 +14,13 @@ pub async fn get_matches(
 ) -> Json<MatchApi> {
     let mut lock = state.username_resolver.lock().await;
     let mut formed_matches = Vec::with_capacity(matches.len());
-    let mut all_players : Vec<Uuid> = Vec::new();
+    let mut all_players : HashSet<Uuid> = HashSet::new();
     for (match_id, data) in matches.iter() {
         all_players.extend(&data.players);
     }
-    let all_username_map = lock.resolve_batch(all_players).await;
+    let all_username_map = lock.resolve_batch(
+        Vec::from_iter(all_players.into_iter())
+    ).await;
     for (id, data) in matches.clone().into_iter() {
         let player_data = {
             let mut player_data_vec: Vec<PlayerData> = Vec::new();
